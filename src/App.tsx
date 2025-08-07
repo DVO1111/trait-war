33504ffd571211f347ae88462c94b2c5a39af913
unknown <aoladehinde887@stu.ui.edu.ng>
2025-08-07 11:52:06 +0100

    fix: resolve merge conflicts and clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import TestProject from "./pages/TestProject";
-import MyProjects from "./pages/MyProjects";
-=======
 import { useMemo } from "react";
 import {
   ConnectionProvider,
@@ -11,8 +7,6 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import {
   WalletModalProvider,
 } from "@solana/wallet-adapter-react-ui";
-
->>>>>>> 05ea7109d9aa30c220981377e75d1c983ff2fa39
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -29,38 +23,14 @@ import Settings from "./pages/Settings";
 import Creator from "./pages/Creator";
 import Blockchain from "./pages/Blockchain";
 import NotFound from "./pages/NotFound";
+import TestProject from "./pages/TestProject";
+import MyProjects from "./pages/MyProjects";
 
 // Default styles for wallet modal
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const queryClient = new QueryClient();
 
-<<<<<<< HEAD
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<WalletAuth />} />
-          <Route path="/" element={<Index />} />
-          <Route path="/missions" element={<GameLayout><Missions /></GameLayout>} />
-          <Route path="/dao" element={<GameLayout><DAO /></GameLayout>} />
-          <Route path="/leaderboard" element={<GameLayout><Leaderboard /></GameLayout>} />
-          <Route path="/creator" element={<GameLayout><Creator /></GameLayout>} />
-          <Route path="/blockchain" element={<GameLayout><Blockchain /></GameLayout>} />
-          <Route path="/settings" element={<GameLayout><Settings /></GameLayout>} />
-          <Route path="/test" element={<GameLayout><TestProject /></GameLayout>} />
-          <Route path="/my-projects" element={<GameLayout><MyProjects /></GameLayout>} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-=======
 const App = () => {
   const network = "https://rpc.test.honeycombprotocol.com";
   const endpoint = useMemo(() => network, [network]);
@@ -88,6 +58,8 @@ const App = () => {
                   <Route path="/creator" element={<GameLayout><Creator /></GameLayout>} />
                   <Route path="/blockchain" element={<GameLayout><Blockchain /></GameLayout>} />
                   <Route path="/settings" element={<GameLayout><Settings /></GameLayout>} />
+                  <Route path="/test" element={<GameLayout><TestProject /></GameLayout>} />
+                  <Route path="/my-projects" element={<GameLayout><MyProjects /></GameLayout>} />
                   {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                   <Route path="*" element={<NotFound />} />
                 </Routes>
@@ -99,6 +71,5 @@ const App = () => {
     </ConnectionProvider>
   );
 };
->>>>>>> 05ea7109d9aa30c220981377e75d1c983ff2fa39
 
 export default App;
